c3550b7966be353b47df6710a26f703469550d25
Sincrono <Sincrono@192.168.137.85>
2018-05-29 12:36:32 +0200

    bean

--- Mercury/src/com/corso/model/Admin.java
+++ Mercury/src/com/corso/model/Admin.java
@@ -6,6 +6,24 @@ public class Admin
 	private String psw;
 	private String user;
 	private String status;
+	public String getPsw() {
+		return psw;
+	}
+	public void setPsw(String psw) {
+		this.psw = psw;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
 	
 	
 }
