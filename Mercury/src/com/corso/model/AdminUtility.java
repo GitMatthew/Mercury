6bd9ca33696a7140edc72aca8ca1c9e4c3bbe6bb
Matteo <Sincrono@192.168.137.165>
2018-05-29 14:35:08 +0200

    modifiche AdminImpl e Utility

--- Mercury/src/com/corso/model/AdminUtility.java
+++ Mercury/src/com/corso/model/AdminUtility.java
@@ -2,21 +2,27 @@ package com.corso.model;
 
 import java.util.*;
 
-public interface AdminUtility 
-{
+public interface AdminUtility {
 
-	public static String CHECK_PSW="SELECT Count(amministratori.user) as tot from amministratori where user like ? and psw like ?";
-	public static String CHECK_EVENT_STATUS="SELECT * FROM eventi WHERE id_status=1";
-	public static String SET_EVENT_STATUS="";
-	public static String SET_NEW_CAT="";
-	public static String MODIFY_CAT="";
-	public static String GET_EVENTS_BY_STATUS="";
-	
-	public boolean login(String user,String psw);
+	public static String CHECK_PSW = "SELECT Count(amministratori.user) as tot from amministratori where user like ? and psw like ?";
+	public static String CHECK_EVENT_STATUS = "SELECT * FROM eventi WHERE id_status=1";
+	public static String SET_EVENT_STATUS = "UPDATE eventi SET id_status=? WHERE id_evento=?";
+	public static String SET_NEW_CAT = "";
+	public static String MODIFY_CAT = "";
+	public static String GET_EVENTS_BY_STATUS = "";
+
+	public boolean login(String user, String psw);
+
 	public void sendNotifica();
+
 	public ArrayList<Evento> getEventiAttesa();
+
 	public ArrayList<Ente> getEntiAttesa();
+
 	public String newCat();
+
 	public String modCat();
-	
+
+	public void modEvento(int id_ev, int id_status);
+
 }
--- Mercury/src/com/corso/model/Login.java
+++ Mercury/src/com/corso/model/Login.java
@@ -11,33 +11,26 @@ import javax.servlet.ServletException;
 import com.corso.connection.Dao;
 
 public class Login {
-	
-	String password_ente = "SELECT * FROM enti WHERE user_ente = ?" ;
-	
-	public boolean verificaPsw (String user , String psw) {	
-		
+
+	String password_ente = "SELECT * FROM enti WHERE user_ente = ?";
+
+	public boolean verificaPsw(String user, String psw) {
+
 		try {
-			
-			Connection conn=Dao.getConnection();
-			
+
+			Connection conn = Dao.getConnection();
+
 			PreparedStatement pst = conn.prepareStatement(password_ente);
-			
+
 			pst.setString(0, "'" + user + "';");
-			
+
 			System.out.println("ok");
-					
-			
+
 		} catch (SQLException e) {
-			
+
 		}
-		
-		
-		
-		
-		
-		
-		
-		return true ;
+
+		return true;
 	}
 
 }
