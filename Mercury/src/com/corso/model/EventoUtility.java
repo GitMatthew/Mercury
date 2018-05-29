84ee6bba5a0e86218a4d71b4fcbb4d69a257ebee
Sincrono <Sincrono@192.168.137.136>
2018-05-29 13:02:18 +0200

    modificatooo

---
+++ Mercury/src/com/corso/model/EventoUtility.java
@@ -0,0 +1,14 @@
+package com.corso.model;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+public interface EventoUtility
+{
+	public String RICERCA_EVENTI = "SELECT distinct e.url_img_evento,  e.nome_evento as titolo, e.descrizione, cat.nome_categoria , com.nome_comune , e.data_inizio, e.data_fine \r\n"
+	        + "from categorie as cat, eventi as e , regioni as r , province as p , comuni as com \r\n"
+	        + "where  e.id_status=2  and e.id_categoria=cat.id_categoria and e.id_comune=com.id_comune \r\n"
+	        + "and com.id_provincia=p.id_provincia and r.id_regione=p.id_regione  ";
+	
+	public ArrayList<Evento> ricerca(String cat, String reg, String prov, String com, Date inizio, Date fine);
+}
