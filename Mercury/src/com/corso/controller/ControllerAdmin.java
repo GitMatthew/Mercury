73e2b544b6ce9ed49b231953f195cade2f3c8f06
Sincrono <Sincrono@192.168.137.85>
2018-05-29 12:45:24 +0200

    controller

--- Mercury/src/com/corso/controller/ControllerAdmin.java
+++ Mercury/src/com/corso/controller/ControllerAdmin.java
@@ -1,11 +1,16 @@
 package com.corso.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.corso.model.AdminImpl;
 
 @WebServlet("/ControllerAdmin")
 public class ControllerAdmin extends HttpServlet {
@@ -22,10 +27,20 @@ public class ControllerAdmin extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String prm=request.getParameter("param");
+		
+		response.setContentType("text/html");
+		
+		AdminImpl im=new AdminImpl();
+		
+		
 	}
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
