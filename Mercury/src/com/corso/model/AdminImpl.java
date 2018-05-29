1c3e615b68e9f80f94fb1a4499893ed591451de7
Matteo <Sincrono@192.168.137.165>
2018-05-29 13:23:37 +0200

    cancellato commenti inutili

--- Mercury/src/com/corso/model/AdminImpl.java
+++ Mercury/src/com/corso/model/AdminImpl.java
@@ -7,43 +7,42 @@ public class AdminImpl implements AdminUtility
 
 	@Override
 	public boolean login() {
-		// TODO Auto-generated method stub
+
 		return false;
 	}
 
 	@Override
 	public void cambiaPsw() {
-		// TODO Auto-generated method stub
+
 		
 	}
 
 	@Override
 	public void sendNotifica() {
-		// TODO Auto-generated method stub
+
 		
 	}
 
 	@Override
 	public ArrayList<Evento> getEventiAttesa() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public ArrayList<Ente> getEntiAttesa() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String newCat() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	@Override
 	public String modCat() {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
