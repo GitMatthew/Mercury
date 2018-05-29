96a164399446e7b4b436f97ce9e751a72e07a94d
Sincrono <Sincrono@192.168.137.85>
2018-05-29 12:26:20 +0200

    impl

--- Mercury/src/com/corso/model/AdminImpl.java
+++ Mercury/src/com/corso/model/AdminImpl.java
@@ -1,5 +1,38 @@
 package com.corso.model;
 
-public class AdminImpl implements AdminUtility {
+import java.util.ArrayList;
+
+public class AdminImpl implements AdminUtility 
+{
+
+	@Override
+	public boolean login() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void cambiaPsw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendNotifica() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Evento> getEventiAttesa() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Ente> getEntiAttesa() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
