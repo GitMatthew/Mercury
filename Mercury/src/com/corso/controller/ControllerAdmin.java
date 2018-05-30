ccf93c23f3bc0ea50b6da50b8af99e9fb04d444f
Matteo <Sincrono@192.168.137.165>
2018-05-30 13:17:31 +0200

    aa

--- Mercury/src/com/corso/controller/ControllerAdmin.java
+++ Mercury/src/com/corso/controller/ControllerAdmin.java
@@ -75,7 +75,7 @@ public class ControllerAdmin extends HttpServlet {
 		
 	}
 
-
+//
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
