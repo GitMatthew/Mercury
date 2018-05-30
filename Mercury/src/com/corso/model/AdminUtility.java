644da2fcd360dc7f0f7ab546a4fc14573740130b
Matteo <Sincrono@192.168.137.165>
2018-05-30 15:10:58 +0200

    aa

--- Mercury/src/com/corso/model/AdminUtility.java
+++ Mercury/src/com/corso/model/AdminUtility.java
@@ -12,7 +12,7 @@ public interface AdminUtility {
 	public static String MODIFY_CAT = "UPDATE categorie SET nome_categoria=? WHERE id_categoria=?";
 	public static String GET_EVENTS_PENDING = "SELECT id_evento,nome_evento,descrizione,data_inizio,data_fine FROM eventi WHERE id_status=1";
 	public static String GET_ENTS_PENDING = "SELECT id_ente,nome_ente,descrizione_ente,email_ente FROM enti WHERE id_status=1";
-	public static String GET_ALL_EVENTS="SELECT id_evento,id_status,nome_evento,descrizione,data_inizio,data_fine FROM eventi WHERE id_status=2 || id_status=3 order by id_status";
+	public static String GET_ALL_EVENTS="SELECT id_evento,id_status,nome_status,nome_evento,descrizione,data_inizio,data_fine FROM eventi WHERE id_status=2 || id_status=3 order by id_status";
 	public static String GET_ALL_ENTS="SELECT id_ente,id_status,nome_ente,descrizione_ente FROM enti WHERE id_status=2 || id_status=3 || id_status=4 order by id_status";
 	public static String GET_ALL_CAT = "SELECT nome_categoria FROM categorie";
 	
