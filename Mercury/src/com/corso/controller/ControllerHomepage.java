3bd42274d4bca181e72ce54efbef799484e5ab5f
andyy <andyy@Andrea-pc.homenet.telecomitalia.it>
2018-06-03 09:29:31 +0200

    altre modifichee :D

--- Mercury/src/com/corso/controller/ControllerHomepage.java
+++ Mercury/src/com/corso/controller/ControllerHomepage.java
@@ -17,50 +17,51 @@ import com.corso.model.EventoImpl;
  * Servlet implementation class ControllerHomepage
  */
 @WebServlet("/ControllerHomepage")
-public class ControllerHomepage extends HttpServlet
-{
-	private static final long serialVersionUID = 1L;
+public class ControllerHomepage extends HttpServlet {
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
 
-		try
-		{
+	try {
 
-			String da = request.getParameter("da");
-			String cat = request.getParameter("categoria");
-			String reg = request.getParameter("regione");
-			String prov = request.getParameter("provincia");
-			String com = request.getParameter("comune");
-			String ini = request.getParameter("dataInizio");
-			String fin = request.getParameter("dataFine");
+	    String da = request.getParameter("da");
+	    String cat = request.getParameter("categoria");
+	    String reg = request.getParameter("regione");
+	    String prov = request.getParameter("provincia");
+	    String com = request.getParameter("comune");
+	    String ini = request.getParameter("dataInizio");
+	    String fin = request.getParameter("dataFine");
 
-			if (da.equals("1"))
-			{
-				EventoImpl ev = new EventoImpl();
-				ArrayList<Evento> RicEv = ev.ricerca(cat, reg, prov, com, ini, fin);
+	    if (da.equals("1")) {
+		EventoImpl ev = new EventoImpl();
+		ArrayList<Evento> RicEv = ev.ricerca(cat, reg, prov, com, ini, fin);
 
-				HttpSession s = request.getSession();
-				s.setAttribute("risultatoRicerca", RicEv);
-				response.sendRedirect("view/homepageMercury.jsp");
+		HttpSession s = request.getSession();
+		s.setAttribute("risultatoRicerca", RicEv);
+		response.sendRedirect("view/homepageMercury.jsp");
 
-			} else if (da.equals("0"))
-			{
+	    } else if (da.equals("0")) {
+		cat = "null";
+		reg = "null";
+		prov = "null";
+		com = "null";
+		ini = "";
+		fin = "";
 
-				EventoImpl ev = new EventoImpl();
-				ArrayList<Evento> RicEv = ev.primiEventi();
+		EventoImpl ev = new EventoImpl();
+		ArrayList<Evento> RicEv = ev.ricerca(cat, reg, prov, com, ini, fin);
 
-				HttpSession s = request.getSession();
-				s.setAttribute("risultatoRicerca", RicEv);
-				response.sendRedirect("view/homepageMercury.jsp");
-			}
+		HttpSession s = request.getSession();
+		s.setAttribute("risultatoRicerca", RicEv);
+		response.sendRedirect("view/homepageMercury.jsp");
+	    }
 
-		} catch (Exception e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+	} catch (Exception e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
+
+    }
 
 }
--- Mercury/src/com/corso/model/EventoUtility.java
+++ Mercury/src/com/corso/model/EventoUtility.java
@@ -2,21 +2,13 @@ package com.corso.model;
 
 import java.util.ArrayList;
 
-public interface EventoUtility
-{
-	public String RICERCA_EVENTI = "SELECT distinct e.url_img_evento,  e.nome_evento as titolo, e.descrizione, cat.nome_categoria , com.nome_comune , e.data_inizio, e.data_fine \r\n"
-	        + "from categorie as cat, eventi as e , regioni as r , province as p , comuni as com \r\n"
-	        + "where  e.id_status=2  and e.id_categoria=cat.id_categoria and e.id_comune=com.id_comune \r\n"
-	        + "and com.id_provincia=p.id_provincia and r.id_regione=p.id_regione  ";
-	
-	public String TUTTI_EVENTI = "\r\n"
-	        + "SELECT  distinct e.id_evento, e.url_img_evento,e.nome_evento as titolo, e.descrizione, cat.nome_categoria , com.nome_comune , e.data_inizio, e.data_fine \r\n"
-	        + "from eventi as e , categorie as cat , comuni as com\r\n"
-	        + "where   e.id_status=2 and  e.id_categoria= cat.id_categoria and e.id_comune=com.id_comune  order by data_inizio asc;";
-	
-	public ArrayList<Evento> ricerca(String cat, String reg, String prov, String com, String inizio, String fine);
-	
-	public ArrayList<Evento> primiEventi();
-	
-	//public ArrayList<Evento> eventiEnte (int id_ente);
+public interface EventoUtility {
+    public String RICERCA_EVENTI = "SELECT distinct e.url_img_evento,  e.nome_evento as titolo, e.descrizione, cat.nome_categoria , com.nome_comune , e.data_inizio, e.data_fine \r\n"
+	    + "from categorie as cat, eventi as e , regioni as r , province as p , comuni as com \r\n"
+	    + "where  e.id_status=2  and e.id_categoria=cat.id_categoria and e.id_comune=com.id_comune \r\n"
+	    + "and com.id_provincia=p.id_provincia and r.id_regione=p.id_regione  ";
+
+    public ArrayList<Evento> ricerca(String cat, String reg, String prov, String com, String inizio, String fine);
+
+    // public ArrayList<Evento> eventiEnte (int id_ente);
 }
