b914aae111dd5fced0e50414a0ff173d279c2ad5
Sincrono <Sincrono@192.168.137.136>
2018-06-05 10:01:01 +0200

    srha<regsr

--- Mercury/src/com/corso/model/EventoImpl.java
+++ Mercury/src/com/corso/model/EventoImpl.java
@@ -8,211 +8,210 @@ import java.util.ArrayList;
 
 import com.corso.connection.Dao;
 
-public class EventoImpl implements EventoUtility {
-
-    @Override
-    public ArrayList<Evento> ricerca(String cat, String reg, String pro, String com, String ini, String fin) {
-
-	String x = "";
-	if (!(cat.equals("null"))) {
-	    x = x + " and cat.nome_categoria= '" + cat + "'";
-	} else {
-	}
-	if (!(reg.equals("null"))) {
-	    x = x + " and r.nome_regione= '" + reg + "'";
-	} else {
-	}
-	if (!(pro.equals("null"))) {
-	    x = x + " and p.nome_provincia= '" + pro + "'"; 
-	} else {
-	}
-	if (!(com.equals("null"))) {
-	    x = x + " and com.nome_comune= '" + com + "'";
-	} else {
-	}
-	if (!(ini.equals(""))) {
-	    x = x + " and e.data_inizio= '" + ini + "'";
-	} else {
-	}
-	if (!(fin.equals(""))) {
-	    x = x + " and e.data_fine= '" + fin + "'";
-	} else {
-	}
-
-	x = x + " order by data_inizio asc;";
-
-	ArrayList<Evento> atList = new ArrayList<Evento>();
-	Evento a = null;
-	Connection conn = Dao.getConnection();
-	try {
-	    Statement pst = conn.createStatement();
-	    ResultSet rst = pst.executeQuery(EventoUtility.RICERCA_EVENTI + x);
-
-	    while (rst.next()) {
-		a = new Evento();
-		a.setUrl_img_evento(rst.getString("url_img_evento"));
-		a.setNome_evento(rst.getString("titolo"));
-		a.setNome_categoria(rst.getString("nome_categoria"));
-		a.setDescrizione(rst.getString("descrizione"));
-		a.setNome_comune(rst.getString("nome_comune"));
-		a.setData_inizio(rst.getDate("data_inizio"));
-		a.setData_fine(rst.getDate("data_fine"));
-		atList.add(a);
-	    }
-	} catch (SQLException e) {
-	    e.printStackTrace();
-	}
-
-	finally {
-	    try {
-		conn.close();
-	    } catch (SQLException e) {
-		e.printStackTrace();
-	    }
-	}
-	return atList;
-
-    }
-
-	public static ArrayList<Evento> eventiEnte(int id_ente) {
+public class EventoImpl implements EventoUtility
+{
+	
+	@Override
+	public ArrayList<Evento> ricerca(String cat, String reg, String pro, String com, String ini, String fin)
+	{
+		
+		String x = "";
+		if (!(cat.equals("null"))) {
+			x = x + " and cat.nome_categoria= '" + cat + "'";
+		}
+		else {
+		}
+		if (!(reg.equals("null"))) {
+			x = x + " and r.nome_regione= '" + reg + "'";
+		}
+		else {
+		}
+		if (!(pro.equals("null"))) {
+			x = x + " and p.nome_provincia= '" + pro + "'";
+		}
+		else {
+		}
+		if (!(com.equals("null"))) {
+			x = x + " and com.nome_comune= '" + com + "'";
+		}
+		else {
+		}
+		if (!(ini.equals(""))) {
+			x = x + " and e.data_inizio= '" + ini + "'";
+		}
+		else {
+		}
+		if (!(fin.equals(""))) {
+			x = x + " and e.data_fine= '" + fin + "'";
+		}
+		else {
+		}
+		
+		x = x + " order by data_inizio asc;";
+		
 		ArrayList<Evento> atList = new ArrayList<Evento>();
 		Evento a = null;
 		Connection conn = Dao.getConnection();
-		String qry = EVENTI_ENTE + " and e.id_ente = '"+id_ente+"'";
 		try {
-		    Statement pst = conn.createStatement();
-		    ResultSet rst = pst.executeQuery(qry);
-
-		    while (rst.next()) {
-			a = new Evento();
-			a.setUrl_img_evento(rst.getString("url_img_evento"));
-			a.setNome_evento(rst.getString("nome_evento"));
-			a.setNome_categoria(rst.getString("nome_categoria"));
-			a.setDescrizione(rst.getString("descrizione"));
-			a.setNome_comune(rst.getString("nome_comune"));
-			a.setNome_status(rst.getString("nome_status"));
-			a.setData_inizio(rst.getDate("data_inizio"));
-			a.setData_fine(rst.getDate("data_fine"));
-			a.setId_evento(rst.getInt("id_evento"));
-			atList.add(a);
-		    }
-		    
-		} catch (SQLException e) {
-		    e.printStackTrace();
+			Statement pst = conn.createStatement();
+			ResultSet rst = pst.executeQuery(EventoUtility.RICERCA_EVENTI + x);
+			
+			while (rst.next()) {
+				a = new Evento();
+				a.setUrl_img_evento(rst.getString("url_img_evento"));
+				a.setNome_evento(rst.getString("titolo"));
+				a.setNome_categoria(rst.getString("nome_categoria"));
+				a.setDescrizione(rst.getString("descrizione"));
+				a.setNome_comune(rst.getString("nome_comune"));
+				a.setData_inizio(rst.getDate("data_inizio"));
+				a.setData_fine(rst.getDate("data_fine"));
+				atList.add(a);
+			}
 		}
-
-		finally {
-		    try {
-			conn.close();
-		    } catch (SQLException e) {
+		catch (SQLException e) {
 			e.printStackTrace();
-		    }
 		}
-		return atList; 
-
-	    }
+		
+		finally {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return atList;
+		
+	}
+	
+	public static ArrayList<Evento> eventiEnte(int id_ente)
+	{
+		ArrayList<Evento> atList = new ArrayList<Evento>();
+		Evento a = null;
+		Connection conn = Dao.getConnection();
+		String qry = EVENTI_ENTE + " and e.id_ente = '" + id_ente + "'";
+		try {
+			Statement pst = conn.createStatement();
+			ResultSet rst = pst.executeQuery(qry);
+			
+			while (rst.next()) {
+				a = new Evento();
+				a.setUrl_img_evento(rst.getString("url_img_evento"));
+				a.setNome_evento(rst.getString("nome_evento"));
+				a.setNome_categoria(rst.getString("nome_categoria"));
+				a.setDescrizione(rst.getString("descrizione"));
+				a.setNome_comune(rst.getString("nome_comune"));
+				a.setNome_status(rst.getString("nome_status"));
+				a.setData_inizio(rst.getDate("data_inizio"));
+				a.setData_fine(rst.getDate("data_fine"));
+				a.setId_evento(rst.getInt("id_evento"));
+				atList.add(a);
+			}
+			
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		finally {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return atList;
+		
+	}
 	
 	@Override
-    public ArrayList<Evento> filtroProvince(String reg)
-    {
-
-	String x = "";
-
-	if (!(reg.equals("null")))
+	public ArrayList<String> filtroProvince(String reg)
 	{
-	    x = x + " and r.nome_regione= '" + reg + "'";
-	} else
-	{
+		System.out.println(reg);
+		String x = "";
+		if (!(reg.equals("null"))) {
+			x = x + " and r.nome_regione= '" + reg + "'";
+			
+			x = x + " order by p.nome_provincia;";
+			
+			ArrayList<String> atList = new ArrayList<String>();
+			
+			Connection conn = Dao.getConnection();
+			try {
+				Statement pst = conn.createStatement();
+				ResultSet rst = pst.executeQuery(EventoUtility.FILTRO_PROVINCE + x);
+				
+				while (rst.next()) {
+					atList.add(rst.getString("nome_provincia"));
+					
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			finally {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			return atList;
+		}
+		return null;
 	}
-
-	x = x + " order by p.nome_provincia;";
-
-	ArrayList<Evento> atList = new ArrayList<Evento>();
-	Evento a = null;
-	Connection conn = Dao.getConnection();
-	try
+	
+	@Override
+	public ArrayList<String> filtroComuni(String pro)
 	{
-	    Statement pst = conn.createStatement();
-	    ResultSet rst = pst.executeQuery(EventoUtility.FILTRO_PROVINCE + x);
-
-	    while (rst.next())
-	    {
-		a = new Evento();
-		a.setNome_provincia(rst.getString("nome_provincia"));
-		atList.add(a);
-	    }
-	} catch (SQLException e)
-	{
-	    e.printStackTrace();
+		String x = "";
+		
+		if (!(pro.equals("null"))) {
+			x = x + " and p.nome_provincia= '" + pro + "'";
+			
+			x = x + " order by c.nome_comune;";
+			
+			ArrayList<String> atList = new ArrayList<String>();
+			
+			Connection conn = Dao.getConnection();
+			try {
+				Statement pst = conn.createStatement();
+				ResultSet rst = pst.executeQuery(EventoUtility.FILTRO_COMUNI + x);
+				
+				while (rst.next()) {
+					atList.add(rst.getString("nome_comune"));
+				}
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			finally {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			return atList;
+		}
+		return null;
 	}
-
-	finally
+	
+	public static Evento ricercaID(String id_evento)
 	{
-	    try
-	    {
-		conn.close();
-	    } catch (SQLException e)
-	    {
-		e.printStackTrace();
-	    }
-	}
-	return atList;
-    }
-
-    @Override
-    public ArrayList<Evento> filtroComuni(String pro)
-    {
-	String x = "";
-
-	if (!(pro.equals("null")))
-	{
-	    x = x + " and p.nome_provincia= '" + pro + "'";
-	} else
-	{
-	}
-	x = x + " order by c.nome_comune;";
-
-	ArrayList<Evento> atList = new ArrayList<Evento>();
-	Evento a = null;
-	Connection conn = Dao.getConnection();
-	try
-	{
-	    Statement pst = conn.createStatement();
-	    ResultSet rst = pst.executeQuery(EventoUtility.FILTRO_COMUNI + x);
-
-	    while (rst.next())
-	    {
-		a = new Evento();
-		a.setNome_comune(rst.getString("nome_comune"));
-		atList.add(a);
-	    }
-	} catch (SQLException e)
-	{
-	    e.printStackTrace();
-	}
-
-	finally
-	{
-	    try
-	    {
-		conn.close();
-	    } catch (SQLException e)
-	    {
-		e.printStackTrace();
-	    }
-	}
-	return atList;
-    }
-    
-    public static Evento ricercaID (String id_evento) {
-    	Evento a = new Evento();
+		Evento a = new Evento();
 		Connection conn = Dao.getConnection();
-		String qry = "SELECT * FROM eventi WHERE id_evento = "+id_evento;
+		String qry = "SELECT * FROM eventi WHERE id_evento = " + id_evento;
 		try {
-		    Statement pst = conn.createStatement();
-		    ResultSet rst = pst.executeQuery(qry);
-
-		    rst.next();
+			Statement pst = conn.createStatement();
+			ResultSet rst = pst.executeQuery(qry);
+			
+			rst.next();
 			a = new Evento();
 			a.setUrl_img_evento(rst.getString("url_img_evento"));
 			a.setNome_evento(rst.getString("nome_evento"));
@@ -222,25 +221,23 @@ public class EventoImpl implements EventoUtility {
 			a.setId_status(rst.getInt("id_status"));
 			a.setData_inizio(rst.getDate("data_inizio"));
 			a.setData_fine(rst.getDate("data_fine"));
-			a.setId_evento(rst.getInt("id_evento"));		    
-		    
-		} catch (SQLException e) {
-		    e.printStackTrace();
+			a.setId_evento(rst.getInt("id_evento"));
+			
 		}
-
-		finally {
-		    try {
-			conn.close();
-		    } catch (SQLException e) {
+		catch (SQLException e) {
 			e.printStackTrace();
-		    }
 		}
-		return a; 
-
-	    
-    	
-    }
-    
-    
-
+		
+		finally {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return a;
+		
+	}
+	
 }
