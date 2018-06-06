2bb7b6cb9fee53ba4c15782721d555314b173fb5
Matteo <Sincrono@192.168.137.165>
2018-06-06 14:49:46 +0200

    github fa schifo

--- Mercury/src/com/corso/controller/ControllerNewsletter.java
+++ Mercury/src/com/corso/controller/ControllerNewsletter.java
@@ -1,10 +1,12 @@
 package com.corso.controller;
 
 import java.io.IOException;
+
 import java.util.ArrayList;
 
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,7 +19,7 @@ import com.corso.model.Luogo;
 import com.corso.model.NewsletterImpl;
 import com.corso.model.Provincia;
 import com.corso.model.Regione;
-
+@WebServlet("/ControllerNewsletter")
 public class ControllerNewsletter extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
